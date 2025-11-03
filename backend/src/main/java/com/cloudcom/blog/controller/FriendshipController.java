8ab7f160f84a9ae018dad46223e38fdfea4defb0
Lying <3191346893@qq.com>
2025-11-03 14:18:47 +0800

    fix: 修复 FriendshipController 导入问题
    - 将 javax.servlet 改为 jakarta.servlet
    - 适配 Spring Boot 3.x (Jakarta EE 9+)
    - 解决 CI 编译错误

--- backend/src/main/java/com/cloudcom/blog/controller/FriendshipController.java
+++ backend/src/main/java/com/cloudcom/blog/controller/FriendshipController.java
@@ -7,7 +7,7 @@ import com.cloudcom.blog.service.FriendshipService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
